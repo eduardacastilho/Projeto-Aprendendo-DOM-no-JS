0a9c4130905d2c6b281ab8ecf12a12788ec9181f
eduardacastilho <eduardacasti09@gmail.com>
2026-01-11 21:13:57 -0300

    Atividade CURSO FRONT END
    Enunciados: 
    Exercício 1 — Troca de Imagem
    
    Crie uma página com uma imagem e dois botões:
    Um botão deve trocar a imagem para outra diferente.
    O outro botão deve restaurar a imagem original.
    _________________________________________________________________________________
    
    
    Exercício 2 — Mostrar e Esconder Texto
    Crie um parágrafo com algum texto.
    Faça dois botões: um deve esconder o texto (ficar invisível).
    O outro deve mostrar o texto novamente.
    _________________________________________________________________________________
    
    
    Exercício 3 — Calculadora Simples
    Crie: 
    Dois campos <input> de número
    Um botão “Somar”.
    Quando clicar no botão, mostre na tela o resultado da soma dos dois números.

---
+++ exercicio.js
@@ -0,0 +1,29 @@
+const newImagePath = './IMAGES/campo.atividade.jpg';
+const originalImagePath = './IMAGES/mar.atividade.jpg';
+const imageElement = document.getElementById('my-image');
+
+function changeImage() {
+   
+    imageElement.src = newImagePath;
+    imageElement.alt= "Nova Imagem";
+}
+
+function restoreImage() {
+    imageElement.src = originalImagePath;
+    imageElement.alt= "Imagem Original";
+}   
+
+function hideText() {
+    document.getElementById('my-text').style.display = 'none';
+}
+function showText() {
+    const textElement = document.getElementById('my-text');
+    textElement.style.display = 'block';
+}
+
+function sumNumbers() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 + num2;
+    document.getElementById('result').innerText = `Resultado: ${result}`;
+}
